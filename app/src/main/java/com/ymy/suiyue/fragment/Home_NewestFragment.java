ec4afa392fd11e0ed5a920ed3a96ee6f1365d228
github635707218 <635707218@qq.com>
2017-02-20 18:05:56 +0800

    DK建好了首页推荐的碎片

--- app/src/main/java/com/ymy/suiyue/fragment/Home_NewestFragment.java
+++ app/src/main/java/com/ymy/suiyue/fragment/Home_NewestFragment.java
@@ -4,16 +4,14 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ymy.suiyue.R;
-
-import com.ymy.suiyue.constants.InterfaceUri;
 import com.ymy.suiyue.adapter.MyNewestAdapter;
 import com.ymy.suiyue.bean.NewestInformation;
+import com.ymy.suiyue.constants.InterfaceUri;
 import com.ymy.suiyue.util.TimeUtils;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
@@ -59,7 +57,6 @@ public class Home_NewestFragment extends Fragment {
 
                     @Override
                     public void onResponse(String response, int id) {
-                        Log.i("info", "onResponse: "+response);
                         try {
                             JSONObject jsonObject = new JSONObject(response);
                             JSONObject object = jsonObject.getJSONObject("data");
