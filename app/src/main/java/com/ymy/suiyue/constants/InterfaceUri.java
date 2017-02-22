038dd22723c97ad3372850d7824aec1a79626295
templeyan <templeyan@163.com>
2017-02-22 14:13:05 +0800

    AndroidManifest

--- app/src/main/java/com/ymy/suiyue/constants/InterfaceUri.java
+++ app/src/main/java/com/ymy/suiyue/constants/InterfaceUri.java
@@ -7,4 +7,12 @@ package com.ymy.suiyue.constants;
 
 public class InterfaceUri {
     public static String newest = "https://api.suiyueyule.com/1.0.2/feed/new?limit=20&page=1";
+    public static String find_hot_musicstar6 ="https://api.suiyueyule.com/1.0.2/feed/star";
+    public static String find_hot_screamlist3 ="https://api.suiyueyule.com/1.0.2/feed/ranklist?lang=utf8&page=1&limit=3";
+    public static String find_hot_newmusic6 = "https://api.suiyueyule.com/1.0.2/discover/hot_musician?page=1&limit=6";
+    public static String find_hot_music_classification6 = "https://api.suiyueyule.com/1.0.2/discover/hot_style";
+
+    public static String find_hot_worklist = "https://api.suiyueyule.com/1.0.2/feed/ranklist-new?limit=10&page=1";
+    public static String find_hot_musicstar100 ="https://api.suiyueyule.com/1.0.2/feed/star?lang=utf8&page=1&limit=100\n";
+
 }
